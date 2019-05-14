ad38f547a172ad6ab3c51520f81a292730634de2
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-05-13 23:39:37 -0300

    create UserController to store new users in mongodb

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const express = require('express')
 
+const UserController = require('./app/controllers/UserController')
+
 const routes = express.Router()
+
+routes.post('/users', UserController.store)
 
 module.exports = routes
