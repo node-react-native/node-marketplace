e62560e5f87a030b7629c8d006a4c364f305108f
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-05-14 23:25:15 -0300

    verifying token

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,14 @@ const express = require('express')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
 
 const routes = express.Router()
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+routes.get('/test', authMiddleware, (req, res) => {
+  res.json({ ok: true })
+})
 
 module.exports = routes
