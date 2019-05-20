ee90ab4de3b46d6b1300c23c62596cef3570444f
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-05-19 21:59:02 -0300

    create Ad schema

---
+++ src/app/models/Ad.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const AdSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Ad', AdSchema)
